dac3d8d46faeaa418343606c4869381a472b4f10
durk <103585220+durkisneer1@users.noreply.github.com>
2026-05-21 06:49:51 -0500

    Update build.sh

--- railway/build.sh
+++ railway/build.sh
@@ -5,6 +5,6 @@ if [ ! -f .env ]; then
   cp .env.example .env
 fi
 
-php artisan key:generate --ansi
+APP_URL=http://localhost php artisan key:generate --ansi
 APP_URL=http://localhost php artisan package:discover --ansi
 APP_URL=http://localhost pnpm exec vite build
