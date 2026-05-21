93b775dbabe00d9e8c2b42adf1104e5c9e5e4229
durk <103585220+durkisneer1@users.noreply.github.com>
2026-05-21 06:54:45 -0500

    same app url override for other sh

--- railway/init-app.sh
+++ railway/init-app.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 set -e
 
-php artisan migrate --force
-php artisan config:cache
-php artisan route:cache
-php artisan view:cache
+APP_URL=http://localhost php artisan migrate --force
+APP_URL=http://localhost php artisan config:cache
+APP_URL=http://localhost php artisan route:cache
+APP_URL=http://localhost php artisan view:cache
