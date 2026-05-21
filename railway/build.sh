3bdd2309cb62cd9d449529e5f5fde673053e3186
durk <103585220+durkisneer1@users.noreply.github.com>
2026-05-21 18:13:46 -0500

    maybe don't need them anymore

--- railway/build.sh
+++ railway/build.sh
@@ -5,6 +5,6 @@ if [ ! -f .env ]; then
   cp .env.example .env
 fi
 
-APP_URL=http://localhost php artisan key:generate --ansi
-APP_URL=http://localhost php artisan package:discover --ansi
-APP_URL=http://localhost pnpm exec vite build
+php artisan key:generate --ansi
+php artisan package:discover --ansi
+pnpm exec vite build
